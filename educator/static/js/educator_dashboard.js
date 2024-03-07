45cda261bd07920c7c4b84b5111f4c33f0767a82
Bigizic <olalekanisaac75@gmail.com>
2024-03-07 23:11:08 +0000

    update educator dashboard.html

--- educator/static/js/educator_dashboard.js
+++ educator/static/js/educator_dashboard.js
@@ -16,7 +16,11 @@ $(document).ready(function() {
         examsDetailToogle.forEach((element) => {
           element.find('span').removeClass('new_input_span').css('width', '0');
           $(`.exam-box .${element.attr('class')}_exam`).css('width', '0');
+          const removeInputTagColor = $(element).find('input');
+          removeInputTagColor.css('color', 'black');
         });
+        const inputTag = $(e).find('input');
+        inputTag.css('color', '#CADA48');
         setTimeout(() => {
           e.find('span').addClass('new_input_span').css('width', '100%');
           $(`.exam-box .${e.attr('class')}_exam`).css('width', '100%');
@@ -34,13 +38,31 @@ $(document).ready(function() {
       if ($(this).find('.new_input_span').width() === 0) {
         $(this).find('.new_input_span').css('width', '100%');
       }
+      const inputTag = $(this).find('input');
+      inputTag.css('color', '#CADA48');
     });
 
   // get width for information box
   const scrollInformationWidth = $('.scroll_information').width();
   const scollInformationWordWidth = $('.scroll_information_words').width();
   const gapWidth = (scrollInformationWidth - scollInformationWordWidth) / 1.5;
-  console.log(scrollInformationWidth, scollInformationWordWidth)
-  console.log(gapWidth)
   $('.scroll_information').css('gap',  `${gapWidth}px`);
+
+  // left side bar toogle
+
+  $(document).on('click', '.menu-button', function () {
+    const closeBUtton = $('<span class="close-button">&times;</span>');
+    $('.right').css('margin-left', '14%');
+    $('.first_ul a').css('display', 'block');
+    $('.second_ul a').css('display', 'block');
+    $(this).replaceWith(closeBUtton);
+  });
+
+  $(document).on('click', '.close-button', function () {
+    const menuBUtton = $('<button class="menu-button">&#9776;</button>');
+    $('.right').css('margin-left', '5%');
+    $('.first_ul a').css('display', 'none');
+    $('.second_ul a').css('display', 'none');
+    $(this).replaceWith(menuBUtton);
+  });
 })
